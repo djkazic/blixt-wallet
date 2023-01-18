5a963b3bbadc7162a62d7d0a66d6ea15668a30b1
djkazic <kevincai96@gmail.com>
2023-01-18 14:54:52 -0500

    Run tor as a foreground service in main Blixt process

--- android/app/src/main/java/com/blixtwallet/BlixtTor.java
+++ android/app/src/main/java/com/blixtwallet/BlixtTor.java
@@ -1,15 +1,23 @@
 package com.blixtwallet;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 import java.util.Stack;
+
+import androidx.core.app.NotificationManagerCompat;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -22,7 +30,6 @@ public class BlixtTor extends ReactContextBaseJavaModule {
   static private final String TAG = "BlixtTor";
   static TorService torService;
   static String currentTorStatus = TorService.STATUS_OFF;
-  static boolean torServiceBound = false;
   static Stack<Promise> calleeResolvers = new Stack<>();
 
   static private final ServiceConnection torServiceConnection = new ServiceConnection() {
@@ -31,14 +38,34 @@ public class BlixtTor extends ReactContextBaseJavaModule {
       // We've bound to LocalService, cast the IBinder and get LocalService instance
       TorService.LocalBinder binder = (TorService.LocalBinder) service;
       torService = binder.getService();
-      torServiceBound = true;
+      Log.i(TAG, "torService.getService()");
+      torService.startForeground(0xc0feefee, getNotification(torService));
       Log.i(TAG, "onServiceConnected");
     }
 
     @Override
     public void onServiceDisconnected(ComponentName arg0) {
-      torServiceBound = false;
       Log.i(TAG, "onServiceDisconnected");
+    }
+
+    public Notification getNotification(Context context) {
+      Intent notificationIntent = new Intent (context, MainActivity.class);
+      PendingIntent pendingIntent =
+        PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE);
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        NotificationChannel channel = new NotificationChannel("com.blixtwallet", "blixt", NotificationManager.IMPORTANCE_NONE);
+        channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.createNotificationChannel(channel);
+        notificationManager.createNotificationChannel(channel);
+      }
+      return new Notification.Builder(context, "com.blixtwallet")
+        .setContentTitle("Blixt Wallet")
+        .setContentText("Blixt Wallet is running in the background")
+        .setSmallIcon(R.drawable.ic_launcher_background)
+        .setContentIntent(pendingIntent)
+        .setTicker("Blixt Wallet")
+        .build();
     }
   };
 
@@ -53,6 +80,8 @@ public class BlixtTor extends ReactContextBaseJavaModule {
         while (calleeResolvers.size() > 0) {
           calleeResolvers.pop().resolve(TorService.socksPort);
         }
+      } else if (status.equals(TorService.STATUS_OFF)) {
+        getReactApplicationContext().unregisterReceiver(torBroadcastReceiver);
       }
     }
   };
@@ -73,26 +102,25 @@ public class BlixtTor extends ReactContextBaseJavaModule {
 //      promise.reject(TAG, "Tor already in progress starting");
 //      return;
 //    }
-    if (torServiceBound || currentTorStatus.equals(TorService.STATUS_ON)) {
-      Log.i(TAG, "torServiceBound: " + torServiceBound + " currentTorStatus.equals(TorService.STATUS_ON)" + currentTorStatus.equals(TorService.STATUS_ON));
+    if (currentTorStatus.equals(TorService.STATUS_ON)) {
+      Log.i(TAG, "currentTorStatus.equals(TorService.STATUS_ON) " + currentTorStatus.equals(TorService.STATUS_ON));
       promise.resolve(TorService.socksPort);
       return;
     }
-    Log.i(TAG, "KOMMER HIT wat" + torServiceBound + " " + currentTorStatus);
+    Log.i(TAG, "KOMMER HIT wat " + currentTorStatus);
     calleeResolvers.add(promise);
     
     getReactApplicationContext().registerReceiver(torBroadcastReceiver, new IntentFilter(TorService.ACTION_STATUS));
     Intent intent = new Intent(getReactApplicationContext(), TorService.class);
+    intent.setAction(TorService.ACTION_START);
+    getReactApplicationContext().startForegroundService(intent);
     getReactApplicationContext().bindService(intent, torServiceConnection, Context.BIND_AUTO_CREATE);
   }
 
   @ReactMethod
   public void stopTor(Promise promise) {
-    if (torServiceBound) {
-      Log.i(TAG,"Unbinding TorService");
-      getReactApplicationContext().unregisterReceiver(torBroadcastReceiver);
-      getReactApplicationContext().unbindService(torServiceConnection);
-    }
+    Log.i(TAG,"Unbinding TorService");
+    getReactApplicationContext().unbindService(torServiceConnection);
     promise.resolve(true);
   };
 }
